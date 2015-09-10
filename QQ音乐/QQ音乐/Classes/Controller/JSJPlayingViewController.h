a8ef38485b6c8b72c97ff95c325cd5e154b1d128
mada <mada@madadeMacBook-Pro.local>
2015-09-10 23:03:36 +0800

    1.初始化项目 2.利用storyboard文件布局播放界面

---
+++ QQ音乐/QQ音乐/Classes/Controller/JSJPlayingViewController.h
@@ -0,0 +1,13 @@
+//
+//  JSJPlayingViewController.h
+//  QQ音乐
+//
+//  Created by mada on 15/9/10.
+//  Copyright (c) 2015年 MD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSJPlayingViewController : UIViewController
+
+@end
---
+++ QQ音乐/QQ音乐/Classes/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQ音乐
+//
+//  Created by mada on 15/9/10.
+//  Copyright (c) 2015年 MD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
